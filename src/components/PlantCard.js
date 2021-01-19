d4c14f680b3f5cd829d640c82078ed1536c4f431
wickedvic <viksipart@gmail.com>
2021-01-18 20:53:00 -0500

    finished core delivs

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard( {plant} ) {
+  const [isInStock, setIsInStock] = useState(true); 
+  const { name, image, price } = plant;
+
+  function handleToggleStock() {
+    setIsInStock(!isInStock);
+  }
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+      {isInStock ? (
+        <button className="primary" onClick={handleToggleStock}>
+          In Stock
+        </button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleToggleStock}>Out of Stock</button>
       )}
     </li>
   );
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const[searchTerm, setSearchTerm] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then(r => r.json())
+      .then(plantsArray => {
+        setPlants(plantsArray)
+      });
+  }, [])
+
+function handleAddPlant(newPlant){
+  const updatedPlantsArray = [...plants, newPlant]
+  setPlants(updatedPlantsArray)
+}
+
+const displayedPlants = plants.filter(plant => {
+  return plant.name.toLowerCase().includes(searchTerm.toLowerCase());
+});
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant}/>
+      <Search searchTerm={searchTerm} onSearchChange={setSearchTerm}/>
+      <PlantList plants={displayedPlants}/>
     </main>
   );
 }
